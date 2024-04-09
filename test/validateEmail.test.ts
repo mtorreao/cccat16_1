13f943e321fe5a6cc666b8a0f4e8e2ded8de6b3a
Matheus Torreão <mtorreao1@gmail.com>
2024-04-09 08:35:05 -0300

    Initial commit

---
+++ test/validateEmail.test.ts
@@ -0,0 +1,25 @@
+import validateEmail from "../src/validators/validateEmail";
+
+test("Deve retornar true para um email válido", function () {
+	const email = "test@example.com";
+	const isValid = validateEmail(email);
+	expect(isValid).toBe(true);
+});
+
+test("Deve retornar false para um email inválido", function () {
+	const email = "invalidemail";
+	const isValid = validateEmail(email);
+	expect(isValid).toBe(false);
+});
+
+test("Deve retornar false para um email sem o domínio", function () {
+	const email = "test@";
+	const isValid = validateEmail(email);
+	expect(isValid).toBe(false);
+});
+
+test("Deve retornar false para um email sem o nome de usuário", function () {
+	const email = "@example.com";
+	const isValid = validateEmail(email);
+	expect(isValid).toBe(false);
+});
---
+++ test/validateName.test.ts
@@ -0,0 +1,13 @@
+import validateName from "../src/validators/validateName";
+
+test("Deve retornar true para um nome válido", function () {
+	const name = "John Doe";
+	const result = validateName(name);
+	expect(result).toBe(true);
+});
+
+test("Deve retornar false para um nome inválido", function () {
+	const name = "123";
+	const result = validateName(name);
+	expect(result).toBe(false);
+});
